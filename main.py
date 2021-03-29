8b9c6467c62a9cf8b6eb3d26fa4d18fd8bbc879e
soop <110252@eesd.org>
2021-03-29 22:40:59 +0000

    added code

--- main.py
+++ main.py
@@ -4,4 +4,38 @@ import random
 turtle.shape("turtle")
 turtle.speed(0)
 
-turtle.color(red, green, blue)
+for i in range(1000):
+  red = random.randint(0, 255)
+  green = random.randint(0, 255)
+  blue = random.randint(0, 255)
+
+  turtle.color(red, green, blue)
+
+  x = random.randint(-300, 300)
+  y = random.randint(-300, 300)
+  turtle.penup()
+  turtle.goto(x,y)
+  turtle.pendown()
+
+  # Draw a rectangle
+  # length and height to be random, 10 - 100
+  length = random.randint(10, 100)
+  height = random.randint(10, 100)
+
+  turtle.begin_fill()
+  for i in range(1, 3):
+    turtle.forward(length)
+    turtle.right(90)
+    turtle.forward(height)
+    turtle.right(90)
+  turtle.end_fill()
+
+  xx = random.randint(-300, 300)
+  yy = random.randint(-300, 300)
+  turtle.penup()
+  turtle.goto(xx,yy)
+  turtle.pendown()
+  radius = random.randint(10, 50)
+  turtle.begin_fill()
+  turtle.circle(radius)
+  turtle.end_fill()
